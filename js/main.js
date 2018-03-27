4a351f5d032c2e9cf297007bcfb98569732f23dd
Sam <Samraven@olivereckert.com>
2018-03-27 12:21:39 +0200

    Adds jQuery, canvas and pixel calculations

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
 window.onload = onReady;
 
 var video;
+var imgInScreen;
+var ctx;
+var canvas;
 
 function onReady(){
   video = document.getElementById('video');
+  canvas = document.getElementById('videoCanvas');
+
+  ctx = canvas.getContext('2d');
+
 
   if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
         // Not adding `{ audio: true }` since we only want video now
@@ -15,14 +22,14 @@ function onReady(){
         });
     }
 }
-
 function draw(){
   //console.log('draw() called');
 
+  ctx.drawImage(video,0,0, canvas.width, canvas.height); // Use camera as background
 
+  imgInScreen = ctx.getImageData(0,0,canvas.width, canvas.height);
 
-
-
+  updateImageData(imgInScreen);
 
 
 
@@ -30,6 +37,15 @@ function draw(){
   requestAnimFrame(draw);
 }
 
+var pixels = [];
+function updateImageData(imgInScreen){
+
+  for (var i = 0; i < imgInScreen.data.length; i+=4) {
+    var pixel = {r: imgInScreen.data[i], g: imgInScreen.data[i+1], b: imgInScreen.data[i+2], a: imgInScreen.data[i+3]};
+    pixels[i] = pixel;
+  }
+  //console.log(pixels);
+}
 
 var canvas_box = document.getElementById("canvas-box"),
     videoCanvas = document.createElement("videoCanvas"),
@@ -39,8 +55,9 @@ var canvas_box = document.getElementById("canvas-box"),
     poly;
 
 function setupPolyer(){
+  points = new Polyer.WebGL(videoCanvas);
 
-  console.log(panel);
+//  console.log(panel);
 }
 
 // MARK: Animation requests
