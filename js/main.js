4c91afd2e14b1db6cdc8edb4833c5b53ff74337f
Sam <Samraven@olivereckert.com>
2018-03-27 10:56:39 +0200

    Adds draw()

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-window.onload = onReady; // first function call
+window.onload = onReady;
 
 var video;
 
@@ -9,6 +9,34 @@ function onReady(){
         // Not adding `{ audio: true }` since we only want video now
             navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
             video.src = window.URL.createObjectURL(stream); // stream
+            console.log('Camera access granted. Setup initialized.');
+            draw();
         });
     }
 }
+
+function draw(){
+  console.log('draw() called');
+
+
+
+
+
+
+
+
+  // Continuos Refresh of draw()
+  requestAnimFrame(draw);
+}
+
+// MARK: Animation requests
+window.requestAnimFrame = (function(){
+    return  window.requestAnimationFrame       ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame    ||
+        window.oRequestAnimationFrame      ||
+        window.msRequestAnimationFrame     ||
+        function(/* function */ callback, /* DOMElement */ element){
+            window.setTimeout(callback, 1000 / 60);
+        };
+})();
